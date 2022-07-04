16638559b185a508fda476ff2713e180dc06a5d6
syndwar <possessed-by-zeal@yandex.ru>
2022-07-04 23:05:43 +0300

    reorganized for engine integration

---
+++ build.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+SET ENGINE_FOLDER="h:\working_dir\stren\"
+
+SET BIN_FOLDER=bin
+SET PROJECT_FOLDER=%~dp0
+
+CD %ENGINE_FOLDER%
+
+CHOICE /M "Press R for Release or D for Debug:" /C RD
+IF %ERRORLEVEL% EQU 1 (
+    CALL build\make_and_install_release_x86.bat
+)
+IF %ERRORLEVEL% EQU 2 (
+    CALL build\make_and_install_debug_x86.bat
+)
+
+IF EXIST %BIN_FOLDER% RMDIR "%BIN_FOLDER%" /s /q
+
+XCOPY "%ENGINE_FOLDER%.install\bin" "%PROJECT_FOLDER%\%BIN_FOLDER%\" /E
+XCOPY "%PROJECT_FOLDER%\src" "%PROJECT_FOLDER%\%BIN_FOLDER%\" /E
+
+pause
